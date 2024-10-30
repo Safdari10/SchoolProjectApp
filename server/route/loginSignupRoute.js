aa5a6b87521e3a79cc7a26fe526ce1822f667c02
Zaker Safdari <zakersafdari@gmail.com>
2024-10-31 12:15:37 +1300

    created routes for both login and signup in route files folder.

--- server/route/loginSignupRoute.js
+++ server/route/loginSignupRoute.js
@@ -1,1 +1,12 @@
  const { getUser, createUser } = require("../controller/loginSignupController")
+ const express = require("express")
+ const router= express.Router();
+
+ //login route
+ router.post("/login", getUser)
+
+ //signup route
+ router.post("/signup", createUser)
+
+
+ module.exports = router
