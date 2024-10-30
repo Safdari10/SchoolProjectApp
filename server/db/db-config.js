e9602064ea42445b79dcfb19fcd1f832bc608fef
Zaker Safdari <zakersafdari@gmail.com>
2024-10-31 09:45:27 +1300

    created connection in db-config and installed mysql2 package for it. created wrapper function for executing parameterised queries.

---
+++ server/db/db-config.js
@@ -0,0 +1,28 @@
+
+const mysql = require("mysql2")
+
+const pool = mysql.createPool({
+   host: process.env.MYSQL_HOST,
+   user: process.env.MYSQL_USER,
+   password: process.env.MYSQL_PASSWORD,
+   database: process.env.MYSQL_DATABASE,
+   waitForConnections: true,
+   connectionLimit: 10,
+   queueLimit: 5,
+})
+
+
+// -- wrapper function for executing parameterized queries --
+
+const query = async (sql, params) => {
+   try {
+      const [results] = await pool.execute(sql, params)
+      return results;
+   } catch (error) {
+      throw new Error('Database query error:' + error.message)
+   }
+}
+
+module.exports = {
+   query
+}
